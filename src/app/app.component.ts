128637c8fb40022144c1040f9ce59f19520fa915
IvanMarchenkoS00273436 <S00273436@atu.ie>
2025-05-09 14:00:33 +0100

    Add footer component with styling and links to the application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { ProductsListComponent } from './common-ui/products-list/products-list.component';
 import { HeaderComponent } from './common-ui/header/header.component';
+import { FooterComponent } from './common-ui/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule, HeaderComponent],
+  imports: [RouterOutlet, CommonModule, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
